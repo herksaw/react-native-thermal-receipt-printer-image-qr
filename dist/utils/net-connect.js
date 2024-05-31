37dee27ac86f9a8a13cf3953b7fb4d44d232342d
Herks Aw <herksaw@gmail.com>
2024-05-31 15:51:25 +0800

    fixed dist imports

--- dist/utils/net-connect.js
+++ dist/utils/net-connect.js
@@ -35,7 +35,7 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     }
 };
 // @ts-ignore
-import Ping from 'react-native-ping';
+import Ping from '@charles-johnson/react-native-ping';
 export var connectToHost = function (ipAddress, timeout) {
     if (timeout === void 0) { timeout = 4000; }
     return new Promise(function (resolve, reject) { return __awaiter(void 0, void 0, void 0, function () {
